9587646df2bb32edc9a2acd9ba7656c6218f5adc
Tri Vo <tri1234562@yahoo.com>
2021-09-23 12:55:40 +0700

    update private key

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const PUBLIC_KEY =
   'pk_test_51JcVQFGekqenOkRviYiKnassRnDxt37pKYWvtKDYOu5J3XRDAJpfJSSjQK8kYEaevtGjivnSEu859lDXPbpsv4ry00QraQqcU9';
 const PRIVATE_KEY =
-  'sk_test_51JcVQFGekqenOkRvRPNVxYiwVFY6YDOcufR1ErsrCCyXMKcqwWOHqr7QIX0j23LahlaKJd7cTxU3KHzvTdMocjtW00GLCuNDnV';
+  '';
 
 const stripe = new Stripe(PRIVATE_KEY, { apiVersion: '2020-08-27' });
 
